91b06d89e81ab3406b5caac38512156907256d6f
MaksimIM <maxminextrem@gmail.com>
2021-09-12 18:00:28 -0700

    Create folders

---
+++ rank-transform/rank-transform.py
@@ -0,0 +1,304 @@
+"""This is a solution for the Rank Transform of a Matrix problem.
+
+This is problem 1632 on LeetCode
+https://leetcode.com/problems/rank-transform-of-a-matrix/
+
+The basic idea is simple:
+going from lowest values to highest,
+for a given value, find groups of cells with that value
+that must have the same rank.
+For a given group, that rank must be
+one more than the maximal ranks already assigned
+to cells in the rows and columns of the group.
+Thus we assign this rank to all cells in the group,
+do this for all groups and then move on to the next value.
+
+A key optimization is to find the groups of cells of same rank
+not by looking for connected components of the  graph whose vertices
+are cells, but, rather, in a graph whose vertices are (row or column)
+indexes. For a fixed value v, a vertex in this graph is any index such
+that in the corresponding row or column there is a cell with value v.
+This cell is then an edge between its row index and its column index.
+All such cells that are edges in a connected component
+of our graph need to be assigned their rank simultaneously.
+
+It is important that for each v we initialize the graph
+to have only the vertices (indexes) that actually contain cells with value v.
+Then the number of vertices will be at most twice the number of edges,
+and the component search will take O(V+E)=O(E) for each value, if done via
+a BFS or a DFS graph exploration algorithm.
+Summing over all values, we see that finding components
+is will take time linear in the number of cells involved, C.
+
+An alternative implementation that uses a union-find sata structure instead.
+When fully optimized, it performs component-finding in O(C alpha(C)) time.
+Here alpha is the inverse Ackermann function,
+which is at most 4 for all practical input sizes
+(an n for which alpha(n)>4 would require more bits to write down
+than there are atoms in the universe).
+
+We provide both implementations via EdgesToComponentsBFS and EdgesToComponentsUF
+classes, which inherit from abstract EdgesToComponentsBase and provide their own
+implementations of the generate_components method. Furthermore, we implement
+both size-based and rank-based union strategies for the union-find version.
+
+Their time performance of all three versions are not too different,
+(the BFS version is a bit faster, landing in 99th percentile on LeetCode),
+though the union-find version uses less memory,
+because it does not need to maintain neighbor-dictionary versions of the graphs,
+For the size-based union strategy the time is close to that of the BFS version.
+
+Overall, the worst-case complexity of each implementation is O(C ln C),
+due to the sorting of the matrix values.
+After that step the rest runs in
+O(C) (BFS)/O(C alpha(C)) (rank-based UF)/O(C ln(C)) (size-based UF)
+time.
+
+Some final notes:
+
+0) To have a fast rank-assignment method, we maintain a lookup table of
+currently maximal ranks in each row/column. The rank of a component is then
+one more than the maximal rank already present.
+1) In the union-find version, to maintain sub-cubic (in N) complexity
+one needs at least one optimization in component finding,
+either merge-by-size or merge by rank, to ensure logarithmic time
+for find and union operations. To obtain the O(C alpha(C)) complexity,
+union by rank with path compression is needed. In practice,
+path compression makes little difference on the LeetCode examples.
+2) There are some alternative approaches to implementing the BFS version.
+For example, instead of the sets of edges, one could directly build
+two dictionaries, one mapping a value to all the indexes relevant to that value
+(aka the vertices of the corresponding graph) and one mapping and index-vertex
+(and a value) to all its neighbors. This has similar performance but would make
+the BFS/DFS implementation diverge more from the union-find one. So we stick to
+the "dictionary of edges" version (and generate neighbours when needed).
+"""
+import copy
+from abc import ABC
+from abc import abstractmethod
+from collections import defaultdict
+from collections import deque
+from typing import List, Iterable, DefaultDict, Hashable, Set
+
+# Choose 'BFS', 'UF_SIZE', 'UF_RANK'
+_VERSION = 'BFS'
+
+
+class Solution:
+    def matrixRankTransform(self, matrix: List[List[int]]) -> List[List[int]]:
+        version_parameters = {
+            'BFS': (EdgesToComponentsBFS,),
+            'UF_SIZE': (EdgesToComponentsUF,
+                        {'strategy_class': ComponentCollectionSizeBased}),
+            'UF_RANK': (EdgesToComponentsUF,
+                        {'strategy_class': ComponentCollectionRankBased}),
+        }
+
+        ranker = Ranker(matrix, *version_parameters[_VERSION])
+        return ranker.solution
+
+
+class Ranker:
+    def __init__(self, matrix, component_maker_class,
+                 component_maker_kwargs=None):
+        self._matrix = matrix
+        self._depth = len(self._matrix)
+        self._width = len(self._matrix[0])
+        self._index_ranks = [0 for _ in range(self._depth+self._width)]
+        self._solution = None
+        self._values = self._create_values()
+        self._edges = self._create_edges()
+        self._components_maker_class = component_maker_class
+        if component_maker_kwargs is None:
+            self._component_maker_kwargs = {}
+        else:
+            self._component_maker_kwargs = component_maker_kwargs
+
+    def _create_values(self):
+        """Produce a sorted list of values that appear in the matrix."""
+        values = set()
+        for i in range(self._depth):
+            for j in range(self._width):
+                values.add(self._matrix[i][j])
+        return sorted(list(values))
+
+    def _create_edges(self):
+        eds = defaultdict(list)
+        for i in range(self._depth):
+            for j in range(self._width):
+                val = self._matrix[i][j]
+                eds[val].append((i, j+self._depth))
+        return eds
+
+    @property
+    def solution(self):
+        if not self._solution:
+            self._solution = [[0 for _ in range(self._width)]
+                              for _ in range(self._depth)]
+            for val in self._values:
+                for component in self._components(val):
+                    self._update_ranks(component)
+                self._assign_ranks(val)
+        return self._solution
+
+    def _components(self, val):
+        return self._components_maker_class(
+            self._edges[val], **self._component_maker_kwargs).components()
+
+    def _update_ranks(self, component):
+        # compute the rank
+        r = max(self._index_ranks[index] for index in component)+1
+        # update the rank
+        for index in component:
+            self._index_ranks[index] = r
+
+    def _assign_ranks(self, val):
+        for i, j_shifted in self._edges[val]:
+            j = j_shifted-self._depth
+            self._solution[i][j] = self._index_ranks[i]
+
+
+class EdgesToComponentsBase(ABC):
+    def __init__(self, edges):
+        self._edges = edges
+        self._vertices = self._get_vertices()
+
+    @abstractmethod
+    def components(self) -> Iterable:
+        pass
+
+    def _get_vertices(self):
+        vertices = set()
+        for i, j in self._edges:
+            vertices.add(i)
+            vertices.add(j)
+        return vertices
+
+
+class EdgesToComponentsBFS(EdgesToComponentsBase):
+
+    def components(self) -> Iterable:
+        """A vanilla bfs component finder method."""
+
+        # Construct the dictionary of neighbours.
+        v_to_nbrs = self._vertex_to_neighbours()
+        # BFS
+        # Copy the vertices or use them up?
+        # We copy, even if using up is a bit faster.
+        remaining_vertices = copy.copy(self._vertices)
+        while remaining_vertices:
+            start = remaining_vertices.pop()
+            q = deque([start])
+            visited = {start}
+
+            while q:
+                v = q.popleft()
+                visited.add(v)
+                for w in v_to_nbrs[v]:
+                    if w not in visited:
+                        q.append(w)
+                        visited.add(w)
+                        remaining_vertices.remove(w)
+            yield visited
+
+    def _vertex_to_neighbours(self) -> DefaultDict[Hashable, Set]:
+        v_to_nbrs = defaultdict(set)
+        for i, j in self._edges:
+            v_to_nbrs[i].add(j)
+            v_to_nbrs[j].add(i)
+        return v_to_nbrs
+
+
+class EdgesToComponentsUF(EdgesToComponentsBase):
+    def __init__(self, edges, strategy_class):
+        super().__init__(edges)
+        self._strategy_class = strategy_class
+
+    def components(self) -> Iterable:
+        """A union-find-based component finder method."""
+
+        # create graph
+        graph = self._strategy_class(self._get_vertices())
+        # add edges
+        for i, j in self._edges:
+            graph.union(i, j)
+        return graph.component_list()
+
+
+class ComponentCollection(ABC):
+    """The union-find data structure.
+
+    Maintains a collection of connected components as an implicit set of trees.
+    Allows finding a representative (root) of a component
+    and merging two components.
+    """
+    def __init__(self, components):
+        self._node_to_parent = {c: c for c in components}
+
+    @abstractmethod
+    def find_root(self, c):
+        pass
+
+    @abstractmethod
+    def union(self, c1, c2):
+        pass
+
+    def component_list(self):
+        components = defaultdict(list)
+        for node in self._node_to_parent:
+            components[self.find_root(node)].append(node)
+        return components.values()
+
+
+class ComponentCollectionRankBased(ComponentCollection):
+    def __init__(self, components):
+        super().__init__(components)
+        self._node_to_rank = {c: 0 for c in components}  # max depth to a leaf
+
+    def find_root(self, c):
+        # with path compression, that is
+        # every root-finding call changes all parents in the path to be the root
+        if self._node_to_parent[c] == c:
+            return c
+        else:  # make parent be the (recursively found) root and return it
+            self._node_to_parent[c] = self.find_root(self._node_to_parent[c])
+        return self._node_to_parent[c]
+
+    def union(self, c1, c2):
+        root1, root2 = self.find_root(c1), self.find_root(c2)
+        if root1 != root2:
+            # make shallower tree a subtree of a deeper one
+            rank1, rank2 = self._node_to_rank[root1], self._node_to_rank[root2]
+            if rank1 > rank2:
+                root1, root2 = root2, root1
+                rank1, rank2 = rank2, rank1
+            self._node_to_parent[root1] = root2
+            # if equally deep, the depth of the union is higher by 1
+            if rank1 == rank2:
+                self._node_to_rank[root2] = rank2+1
+
+
+class ComponentCollectionSizeBased(ComponentCollection):
+    def __init__(self, components):
+        super(ComponentCollectionSizeBased, self).__init__(components)
+        self._parent_to_nodes = {c: {c} for c in components}
+
+    def find_root(self, c):
+        return self._node_to_parent[c]
+
+    def union(self, c1, c2):
+        root1, root2 = self.find_root(c1), self.find_root(c2)
+        if root1 != root2:
+            size1, size2 = len(self._parent_to_nodes[root1]), \
+                           len(self._parent_to_nodes[root2])
+            # make root1 smaller
+            if size1 > size2:
+                root1, root2 = root2, root1
+            # merge trees
+            self._node_to_parent[root1] = root2
+            # repoint all children of root1
+            for child in self._parent_to_nodes[root1]:
+                self._node_to_parent[child] = root2
+            # update children dict
+            self._parent_to_nodes[root2].update(self._parent_to_nodes[root1])
+            del self._parent_to_nodes[root1]
---
+++ sudoku-solver/sudoku-solver.py
@@ -0,0 +1,202 @@
+""" This is a Sudoku solver.
+
+Solving Sudoku is problem 1632 on LeetCode
+https://leetcode.com/problems/sudoku-solver/
+
+The code below implements a backtracking algorithm with the
+"most constrained first" heuristic for choosing which cell to fill.
+
+Namely, we maintain the board as a priority queue of cells,
+ordered by number of possible values that can be put into that cell
+without violating the sudoku constraints.
+(The abstract data type of queue is implemented as an array of sets of cells,
+with index being the number of possible values.
+This is an efficient implementation for our use case,
+since our priorities only take values between 0 and 10.)
+We then try to fill in one of the most constrained cells,
+update the priority queue, and recurse.
+
+The number of operations required to fill a cell
+depends on the number of its unfilled neighbours.
+In the worst cases, this is O(N^2) (where N=3 for usual Sudoku).
+Hence the overall running time is O(N^2 x number of cell-fillings).
+(This is in contrast to O(N^3 x number of cell-fillings) for the queue-less
+version that computes a least-constrained cell "on the fly", or
+O(N^2 log N x number of cell-fillings) for a heap-based queue version).
+In practice the code runs fairly quickly
+(36 to 51 ms on LeetCode, 99th to 95th percentile
+and under 2 seconds on the following challenging test case:
+[
+[".",".",".",".",".",".",".","1","."],
+[".",".",".",".",".","2",".",".","3"],
+[".",".",".","4",".",".",".",".","."],
+[".",".",".",".",".",".","5",".","."],
+["4",".","1","6",".",".",".",".","."],
+[".",".","7","1",".",".",".",".","."],
+[".","5",".",".",".",".","2",".","."],
+[".",".",".",".","8",".",".","4","."],
+[".","3",".","9","1",".",".",".","."]
+]
+).
+
+I have also implemented the "least constraining value first"
+for choosing which values to try.
+However, it did not improve performance, an so is disabled in the code below.
+"""
+from typing import List
+
+ALPHABET = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
+N = 3
+
+
+class Cell:
+    def __init__(self, i, j, possible_values=None, neighbours=None):
+        self._i = i
+        self._j = j
+
+        if possible_values is None:
+            self.possible_values = set(ALPHABET)
+        else:
+            self.possible_values = possible_values
+        if neighbours is None:
+            self.neighbours = set()
+        else:
+            self.neighbours = neighbours
+
+    @property
+    def i(self):
+        return self._i
+
+    @property
+    def j(self):
+        return self._j
+
+    def ordered_possible_values(self):
+        if len(self.possible_values) < 2:
+            return self.possible_values
+        return sorted(self.possible_values, key=self._how_constraining)
+
+    def _how_constraining(self, value):
+        return [(value in nbr.possible_values)
+                for nbr in self.neighbours].count(True)
+
+
+def _all_neighbors(i, j):
+    """Returns indexes of all entries neighboring (i,j) in the sudoku board"""
+    neighbours = [(i, c) for c in range(N * N)] + [(r, j) for r in range(N * N)]
+    k_0, l_0 = N * (i // N), N * (j // N)
+    for dk in range(N):
+        for dl in range(N):
+            neighbours += [(k_0 + dk, l_0 + dl)]
+    return neighbours
+
+
+def _allowed_values(i, j, board_list):
+    """Returns all values that the entry (i,j) may be given
+    without violating the sudoku constraints."""
+    forbidden_values = {board_list[k][l] for k, l in _all_neighbors(i, j)}
+    return {x for x in ALPHABET if x not in forbidden_values}
+
+
+class Sudoku:
+    def __init__(self, board_list):
+        self._board = board_list
+        self._unfilled = self._build_que()
+
+    @property
+    def board(self):
+        return self._board
+
+    def _build_que(self):
+        table = self._table_of_cells()
+        # Processes the table of cells into a queue of cells.
+        board_que = [set() for _ in range(N * N + 1)]
+        for i in range(N * N):
+            for j in range(N * N):
+                cell = table[i][j]
+                if cell:
+                    board_que[len(cell.possible_values)].add(cell)
+        return board_que
+
+    def _table_of_cells(self):
+        """Make a table of cell objects from the board"""
+        table = [[None for _ in range(N * N)] for _ in range(N * N)]
+        for i in range(N * N):
+            for j in range(N * N):
+                if self._board[i][j] == '.':
+                    table[i][j] = Cell(i, j, _allowed_values(i, j, self._board))
+        # Set the neighbors of the cell objects.
+        for i in range(N * N):
+            for j in range(N * N):
+                if table[i][j]:
+                    table[i][j].neighbours = {table[k][l]
+                                              for (k, l) in _all_neighbors(i, j)
+                                              if ((k, l) != (i, j)
+                                              and table[k][l])}
+        return table
+
+    def _fill_cell_neighbours(self, current_cell, tentative_value):
+        """Restrict possible values of neighbors.
+        Record which neighbors are affected for backtracking.
+        Return the set of affected neighbours."""
+        modified_neighbours = set()
+        for nbr in current_cell.neighbours:
+            if tentative_value in nbr.possible_values:
+                modified_neighbours.add(nbr)
+                self._unfilled[len(nbr.possible_values)].remove(nbr)
+                nbr.possible_values.remove(tentative_value)
+                self._unfilled[len(nbr.possible_values)].add(nbr)
+        return modified_neighbours
+
+    def _unfill_cell_neighbours(self, tentative_value, modified_neighbours):
+        for neighbour in modified_neighbours:
+            self._unfilled[len(neighbour.possible_values)].remove(neighbour)
+            neighbour.possible_values.add(tentative_value)
+            self._unfilled[len(neighbour.possible_values)].add(neighbour)
+
+    def solve(self):
+        """Check if done and find a most constrained cell otherwise."""
+        if self._unfilled[0]:
+            return False
+        d = 1
+        while not self._unfilled[d]:
+            d += 1
+            if d == N * N + 1:
+                return True
+        current_cell = self._unfilled[d].pop()
+
+        # Prepare for the recursion.
+        for neighbour in current_cell.neighbours:
+            neighbour.neighbours.remove(current_cell)
+
+        '''Iterate over possible values trying to fill with them.'''
+        for tentative_value in current_cell.possible_values:
+            # Alternatively, iterate over current_cell.ordered_possible_values()
+            # to use the "least constraining" heuristic.
+            modified_neighbours = self._fill_cell_neighbours(current_cell,
+                                                             tentative_value)
+
+            # See if smaller sudoku is solved. If not, undo the changes.
+            if self.solve():
+                self.board[current_cell.i][current_cell.j] = tentative_value
+                return True
+            else:
+                self._unfill_cell_neighbours(tentative_value,
+                                             modified_neighbours)
+
+        # All values failed. This means we should've chosen a different value
+        # for one of the previous cells. We now undo the changes to the
+        # priority queue, to restore state for backtracking.
+        self._unfilled[d].add(current_cell)
+        for neighbour in current_cell.neighbours:
+            neighbour.neighbours.add(current_cell)
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """The main solving method."""
+        the_sudoku = Sudoku(board)
+        the_sudoku.solve()
+        # Problem specification expects variable 'board'
+        # to contain the solved sudoku.
+        board = the_sudoku.board
